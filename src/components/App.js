cdaff59e0544fbd6d22e2ae7aa8ea5d4d210f891
oscarporras02 <oscarrussi@outlook.com>
2021-02-26 22:00:26 -0500

    using callbacks and props to pass information

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
+import { useState } from 'react';
 import '../App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
 function App() {
-  const result = '0';
+  const [result, setResult] = useState('0');
+  const handleChange = () => setResult('1');
+  const updateDisplay = nValue => {
+    setResult(nValue);
+  };
+
   return (
     <>
       <div className="App">
         <div className="content">
           <h1 className="title-app">Welcome To My Calculator</h1>
+          <button type="submit" onClick={handleChange}>click to increase display</button>
           <Display result={result} />
-          <ButtonPanel />
+          <ButtonPanel updateDisplay={updateDisplay} />
         </div>
       </div>
     </>
