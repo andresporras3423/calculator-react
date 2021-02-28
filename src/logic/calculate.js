2197cc887c38da9ef8e122ff98438a43c101dad8
oscarporras02 <oscarrussi@outlook.com>
2021-02-27 20:13:41 -0500

    fix mistakes in the calculations

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -12,27 +12,24 @@ export function calculate(obj1, buttonName) {
     obj.operation = '';
     obj.next = '';
   } else if (buttonName === '+/-') {
-    obj.total *= -1;
-    obj.operation = '';
-    obj.next = '';
+    if (obj.operation === '') obj.total *= -1;
+    else obj.next *= -1;
   } else if (buttonName === '%') {
-    obj.total /= 100;
-    obj.operation = '';
-    obj.next = '';
+    if (obj.operation === '') obj.total /= 100;
+    else obj.next /= 100;
   } else if (buttonName === '.') {
-    if (obj.operation === '' && obj.total.match(/\./) == null) {
-      obj.total = `${obj.total}.0`;
-    } else if (obj.next.match(/\./) === null) {
-      if (obj.next === '') obj.next = '0.0';
-      else obj.next = `${obj.next}.0`;
-    }
+    if (obj.operation === '') {
+      if (obj.total.match(/\./) === null) obj.total = `${obj.total}.0`;
+    } else if (obj.next === '') obj.next = '0.0';
+    else if (obj.next.match(/\./) === null) obj.next = `${obj.next}.0`;
   } else if (buttonName.match(/[1-9]/) !== null) {
     if (obj.operation === '') {
-      if (obj.total === '0') obj.total = buttonName;
-      else obj.total += buttonName;
-    } else {
-      obj.next += buttonName;
-    }
+      if (obj.total === '0' || obj.total.match(/\.[0]+$/)) {
+        obj.total = obj.total.slice(0, obj.total.length - 1) + buttonName;
+      } else obj.total += buttonName;
+    } else if (obj.next === '' || obj.next.match(/\.[0]+$/)) {
+      obj.next = obj.next.slice(0, obj.next.length - 1) + buttonName;
+    } else obj.next += buttonName;
   } else if (buttonName === '0') {
     if (obj.operation === '' && obj.total !== '0') {
       if (obj.total === '0' || obj.total.match(/\.[0]+$/)) {
